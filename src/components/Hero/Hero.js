f8aa8b506101530efcaac1403feef2d709bf58d6
daulen <daulenz@gmail.com>
2023-04-08 10:46:02 -0500

    Hero is added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,25 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome To <br />
+          My Personal Portfolio
+        </SectionTitle>
+        <SectionText>
+        Proficient in MERN stack development (MongoDB, Express, React, Node.js) and a background in the oil and gas sector. Possess strong skills in project & product management that help with accomplishing robust projects. Passionate about coaching, leadership and team-building.
+
+
+        </SectionText>
+        {/* <Button onClick={props.handleClick}>Learn More</Button> */}
+        <Button onClick={()=>window.location='https://pmcoaching.solutions'} > Learn More</Button>
+
+      </LeftSection>
+    </Section>
+  </>
 );
 
 export default Hero;
